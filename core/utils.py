bcf91ef852586087ee5d7920eb60a2436b8177e9
athenas2021 <lucas.kano@outlook.com>
2022-01-08 08:45:36 -0300

    backend scope

---
+++ core/utils.py
@@ -0,0 +1,44 @@
+import requests
+from random import randint
+from django.conf import settings
+from django.contrib.gis.geoip2 import GeoIP2
+from django.contrib.gis.geoip2 import geoip2
+from requests.models import stream_decode_response_unicode
+from requests.utils import dict_from_cookiejar
+
+YELP_SEARCH_ENDPOINT = 'https://api.yeld.com/v3/businesses/search'
+
+def yelp_search(keyword=None, location=None):
+    headers = {"Authorization": "Bearer" + settings.YELP_API_KEY}
+
+    if keyword and location:
+        params = {'term': keyword, 'location': location}
+    else:
+        params = {'term': 'Pizzaria', 'location': 'São Paulo'}
+
+    r = requests.get(YELP_SEARCH_ENDPOINT, headers=headers, params=params)
+    return r.json()
+
+def get_client_city_data():
+    g = GeoIP2
+    ip = get_random_ip()
+    try:
+        return g.city(ip)
+    except geoip2.errors.AddressNotFoundError:
+        return None
+
+def get_random_ip():
+    return '.'.join([str(randint(0,255)) for x in range(4)])
+
+
+""" 
+SHELL TESTING
+
+testando geração de ip randomico
+from core.utils import get_random_ip
+get_random_ip()
+
+testando busca cidade
+from core.utils import get_client_city_data
+ret = get_client_city_data()
+ """
